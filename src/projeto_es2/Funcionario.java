1ce1ceadb094f835cdf18216facf7d7cdd0a094c
Hiago Moura Carvalho <84892686+HiagoMCarvalho@users.noreply.github.com>
2023-11-26 04:55:37 -0300

    Funções CRUD concluidas

--- src/projeto_es2/Funcionario.java
+++ src/projeto_es2/Funcionario.java
@@ -1,3 +1,4 @@
+
 package projeto_es2;
 
 import projeto_es2.Banco.*;
@@ -9,9 +10,8 @@ public class Funcionario
     private String nome;
     private String sobrenome;
     private String funcao;
-    private int cpf;
+    private String cpf;
     private int senha;
-    private int id;
     
     public void addFuncionario(Funcionario F)
     {
@@ -20,7 +20,7 @@ public class Funcionario
         
         try(PreparedStatement smt = conn.prepareStatement(sql))
         {
-            smt.setInt(1, getCpf());
+            smt.setString(1, getCpf());
             smt.setString(2, getNome());
             smt.setString(3, getSobrenome());
             smt.setString(4, getFuncao());
@@ -36,16 +36,185 @@ public class Funcionario
                 
     }
     
-    public Funcionario()
+    public void readFuncionario()
     {
+        Connection conn = Banco.getConnection();
+        String sql = "SELECT * FROM funcionario";
         
+        try(ResultSet rs = conn.createStatement().executeQuery(sql))
+        {
+           while(rs.next())
+           {
+               System.out.println("Funcionario: "+rs.getString("nome")+" " + rs.getString("sobrenome") +" cpf: "+ rs.getString("pkcpf")+" funcao: "+ rs.getString("funcao"));
+           }
+           conn.close();
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }    
     }
-    public Funcionario(String nome, String sobrenome, int cpf, String funcao, int senha) {
+    
+
+    
+        public void readPet()
+    {
+        Connection conn = Banco.getConnection();
+        String sql = "SELECT * FROM pet";
+        
+        try(ResultSet rs = conn.createStatement().executeQuery(sql))
+        {
+           while(rs.next())
+           {
+               System.out.println("Pet: "+rs.getString("nomepet")+" Dono: " + rs.getString("nomeprop") +" Cpf_dono: "+ rs.getString("pkcpfdono")+" Data: "+ rs.getString("pkdata"));
+           }
+           conn.close();
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public void updateNomeFuncionario(Funcionario F, String N)
+    {
+        setNome(N);
+        
+        Connection conn = Banco.getConnection();
+        String sql = "UPDATE funcionario SET nome = ? WHERE pkcpf = ?";
+        
+        try(PreparedStatement smt = conn.prepareStatement(sql))
+        {
+            smt.setString(1, getNome());
+            smt.setString(2, getCpf());
+            ResultSet rs = smt.executeQuery();
+            
+            conn.close();
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+                
+    }
+    
+    public void updateSobrenomeFuncionario(Funcionario F, String N)
+    {
+        setSobrenome(N);
+        
+        Connection conn = Banco.getConnection();
+        String sql = "UPDATE funcionario SET sobrenome = ? WHERE pkcpf = ?";
+        
+        try(PreparedStatement smt = conn.prepareStatement(sql))
+        {
+            smt.setString(1, getSobrenome());
+            smt.setString(2, getCpf());
+            ResultSet rs = smt.executeQuery();
+            
+            conn.close();
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+                
+    }
+    
+    public void updateFuncaoFuncionario(Funcionario F, String N)
+    {
+        setFuncao(N);
+        
+        Connection conn = Banco.getConnection();
+        String sql = "UPDATE funcionario SET funcao = ? WHERE pkcpf = ?";
+        
+        try(PreparedStatement smt = conn.prepareStatement(sql))
+        {
+            smt.setString(1, getFuncao());
+            smt.setString(2, getCpf());
+            ResultSet rs = smt.executeQuery();
+            
+            conn.close();
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+                
+    }
+    
+    public void updateSenhaFuncionario(Funcionario F, int N)
+    {
+        setSenha(N);
+        
+        Connection conn = Banco.getConnection();
+        String sql = "UPDATE funcionario SET senha = ? WHERE pkcpf = ?";
+        
+        try(PreparedStatement smt = conn.prepareStatement(sql))
+        {
+            smt.setInt(1, getSenha());
+            smt.setString(2, getCpf());
+            ResultSet rs = smt.executeQuery();
+            
+            conn.close();
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+                
+    }
+    
+    public void deleteFuncionario()
+    {
+                
+        Connection conn = Banco.getConnection();
+        String sql = "DELETE FROM funcionario WHERE pkcpf = ?";
+        
+        try(PreparedStatement smt = conn.prepareStatement(sql))
+        { 
+            smt.setString(1, getCpf());
+            ResultSet rs = smt.executeQuery();
+            
+            conn.close();
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+             
+    }
+        
+
+    public Funcionario (String cpf) throws SQLException
+    {
+        Connection conn = Banco.getConnection();
+        PreparedStatement smt = conn.prepareStatement("SELECT * FROM funcionario WHERE pkcpf = ?");
+        
+        smt.setString(1, cpf);
+        ResultSet rs = smt.executeQuery();
+        
+        rs.next();
+            
+        this.cpf = rs.getString("pkcpf");
+        this.nome = rs.getString("nome");
+        this.sobrenome = rs.getString("sobrenome");
+        this.funcao = rs.getString("funcao");
+        this.senha = rs.getInt("senha");  
+        
+        conn.close();
+         
+    } 
+
+    public Funcionario(String nome, String sobrenome, String cpf, String funcao, int senha) {
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.cpf = cpf;
         this.funcao = funcao;
         this.senha = senha;
+    }
+
+    public Funcionario()
+    {
+        
     }
     
     
@@ -56,14 +225,6 @@ public class Funcionario
     public void setNome(String nome) {
         this.nome = nome;
     }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
     
     public String getSobrenome() {
         return sobrenome;
@@ -73,11 +234,11 @@ public class Funcionario
         this.sobrenome = sobrenome;
     }
 
-    public int getCpf() {
+    public String getCpf() {
         return cpf;
     }
 
-    public void setCpf(int cpf) {
+    public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
